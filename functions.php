055de0790178feeec2db186fddc9fff5fbb0c331
Ranjeeta <rinkikakodiya148@gmail.com>
2026-03-20 00:01:17 +0530

    work on croping img into site identity section

--- functions.php
+++ functions.php
@@ -2,6 +2,12 @@
 function mytheme_setup() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('custom-logo', array(
+        'height'      => 250,
+        'width'       => 250,
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
 }
 add_action('after_setup_theme', 'mytheme_setup');
